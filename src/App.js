85204328f62765b7c5477b0e5c903ed3f7618826
Saloni Gupta <salonigupta@Salonis-MacBook-Pro.local>
2022-02-14 09:41:03 +0530

    Added frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StudentForm from './StudentForm';
 
-function App() {
+export default function App() {
+  const questions = [
+    {
+      questionText: 'What is the capital of India',
+      answerOptions: [
+        { answerText: 'New Delhi' },
+        { answerText: 'Uttar Pradesh' },
+        { answerText: 'Kolkata' },
+        { answerText: 'Maharashtra' },
+      ],
+      answer: 'New Delhi',
+    },
+    {
+      questionText: 'Who is CEO of Tesla?',
+      answerOptions: [
+        { answerText: 'Jeff Bezos' },
+        { answerText: 'Elon Musk' },
+        { answerText: 'Bill Gates'},
+        { answerText: 'Tony Stark' },
+      ],
+      answer: 'Elon Musk',
+    },
+    {
+      questionText: 'How many Harry Potter books are there?',
+      answerOptions: [
+        { answerText: '1' },
+        { answerText: '4' },
+        { answerText: '6' },
+        { answerText: '7' },
+      ],
+      answer: '7'
+    },
+  ];
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <StudentForm questions={questions} />
     </div>
   );
 }
-
-export default App;
---
+++ src/StudentForm.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+
+const StudentForm = ({questions}) => {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [scoreObtained, setScoreObtained] = useState(0);
+
+  const giveScore = (answer,selectedAnswer) => {
+    console.log(selectedAnswer);
+    console.log(answer);
+    if (answer===selectedAnswer) {
+      setScoreObtained(scoreObtained + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  
+  return (
+    <div>
+      {showScore ? (
+        <>
+          <div>
+                  You scored {scoreObtained} out of {questions.length}
+          </div>
+          <button>Show high scores</button>
+        </>
+      ) : (
+        <>
+          <div >
+            <div >
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div >{questions[currentQuestion].questionText}</div>
+          </div>
+          <br />
+          <div >
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              // eslint-disable-next-line react/jsx-key
+              <button onClick={() => giveScore(questions[currentQuestion].answer,answerOption.answerText)}>{answerOption.answerText}</button>
+            ))}
+            <br />
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default StudentForm
